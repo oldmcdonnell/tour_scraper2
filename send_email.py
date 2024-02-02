c97773d6444ab0bd1e863d7e29d7a69f49d29a0f
oldmcdonnell <oldmcdonnell@gmail.com>
2024-02-01 23:18:32 -0500

    Prior version got riddled with

---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib, ssl
+from var_store import PASSWORD
+import os
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "oldmcdonnell@gmail.com"
+    password = PASSWORD
+
+    receiver = "oldmcdonnell@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+    print("Email was sent")
+
+
+if __name__ == "__main__":
+    send_email("test2")
---
+++ var_store.py
@@ -0,0 +1,1 @@
+PASSWORD = "qyig ztyn hlyf xszz"
